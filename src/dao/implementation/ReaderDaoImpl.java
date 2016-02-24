55dd23343b2140c4d6c561202dfb1d2a41d031b6
LeraNickolaeva <leranick29@gmail.com>
2016-02-24 15:39:45 +0300

    add reader dao

--- src/dao/implementation/ReaderDaoImpl.java
+++ src/dao/implementation/ReaderDaoImpl.java
@@ -4,10 +4,7 @@ import dao.ReaderDao;
 import dao.util.DbUtil;
 import domain.Reader;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,22 +14,70 @@ import java.util.List;
 public class ReaderDaoImpl implements ReaderDao {
     @Override
     public int create(Reader reader) {
-        return 0;
+        int id = 0;
+
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement("insert into `reader` (iduser, email) " +
+                     "values (?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, reader.getIdUser());
+            statement.setString(2, reader.getEmail());
+            statement.execute();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    id = resultSet.getInt(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return id;
     }
 
     @Override
     public Reader read(int idReader) {
-        return null;
+        Reader reader = null;
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement("select * from `reder` where id=?")) {
+            statement.setInt(1, idReader);
+
+            try(ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    reader = new Reader();
+                    reader.setIdUser(resultSet.getInt("iduser"));
+                    reader.setEmail(resultSet.getString("email"));
+                    reader.setId(idReader);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return reader;
     }
 
     @Override
     public void update(Reader reader) {
-
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement("update `reader` set iduser=?, email=? "+
+                     "where id=?")) {
+            statement.setInt(1, reader.getIdUser());
+            statement.setString(2, reader.getEmail());
+            statement.setInt(3, reader.getId());
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(Reader reader) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement("delete from `reader` where id=?")){
+            statement.setInt(1, reader.getId());
+            statement.execute();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
