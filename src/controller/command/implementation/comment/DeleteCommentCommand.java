520f2d2831370d81c9983cfb8f2b753bf57d2c13
happymilk <happymilk853@gmail.com>
2016-03-20 22:23:04 +0300

    add CRUD for comment

---
+++ src/controller/command/implementation/comment/DeleteCommentCommand.java
@@ -0,0 +1,40 @@
+package controller.command.implementation.comment;
+
+import controller.command.Command;
+import dao.CommentDao;
+import dao.implementation.CommentDaoImpl;
+import domain.Comment;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class DeleteCommentCommand implements Command {
+    private static DeleteCommentCommand ourInstance = new DeleteCommentCommand();
+
+    public static DeleteCommentCommand getInstance() {
+        return ourInstance;
+    }
+
+    private DeleteCommentCommand() {
+    }
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        CommentDao commentDao = CommentDaoImpl.getInstance();
+
+        int id = Integer.parseInt(request.getParameter("comment_id"));
+        int idReader = Integer.parseInt(request.getParameter("reader_id"));
+        int idBook = Integer.parseInt(request.getParameter("book_id"));
+        String review = request.getParameter("review");
+
+        Comment comment = new Comment();
+        comment.setId(id);
+        comment.setIdBook(idBook);
+        comment.setReview(review);
+        comment.setIdReader(idReader);
+        commentDao.create(comment);
+
+        commentDao.delete(comment);
+
+        return GetCommentsCommand.getInstance().execute(request);
+    }
+}
---
+++ src/controller/command/implementation/comment/GetCommentsCommand.java
@@ -0,0 +1,34 @@
+package controller.command.implementation.comment;
+
+import controller.command.Command;
+import dao.CommentDao;
+import dao.implementation.CommentDaoImpl;
+import domain.Comment;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public class GetCommentsCommand implements Command {
+    private static GetCommentsCommand ourInstance = new GetCommentsCommand();
+
+    public static GetCommentsCommand getInstance() {
+        return ourInstance;
+    }
+
+    private GetCommentsCommand() {
+    }
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        CommentDao commentDao = CommentDaoImpl.getInstance();
+
+        int idBook = Integer.parseInt(request.getParameter("book_id"));
+
+        List<Comment> comments = commentDao.getCommentsByBookId(idBook);
+
+        request.setAttribute("comments", comments);
+
+        return "book_view.jsp";
+
+    }
+}
