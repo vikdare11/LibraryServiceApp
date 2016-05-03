f0c5e9e5059e8b56940638f5818dddd7b6187e2b
LeraNickolaeva <leranick29@gmail.com>
2016-05-03 21:32:34 +0300

    add struts2 integration for authors actions

--- src/action/author/AddAuthorAction.java
+++ src/action/author/AddAuthorAction.java
@@ -1,4 +1,20 @@
 package action.author;
 
-public class AddAuthorAction {
+import com.opensymphony.xwork2.ActionSupport;
+import controller.command.implementation.author.AddAuthorCommand;
+import org.apache.struts2.ServletActionContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AddAuthorAction extends ActionSupport {
+    public AddAuthorAction() {}
+
+    @Override
+    public String execute() throws Exception {
+        HttpServletRequest request = ServletActionContext.getRequest();
+        if (!AddAuthorCommand.getInstance().execute(request).equals("authors.jsp")) {
+            return ERROR;
+        }
+        return SUCCESS;
+    }
 }
