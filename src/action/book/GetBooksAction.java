ebf2b2cdc8d9789c2d7b7b2f83d1050c684f828c
LeraNickolaeva <leranick29@gmail.com>
2016-05-11 09:28:26 +0300

    add angularJS integration for books actions

--- src/action/book/GetBooksAction.java
+++ src/action/book/GetBooksAction.java
@@ -2,12 +2,26 @@ package action.book;
 
 import com.opensymphony.xwork2.ActionSupport;
 import controller.command.implementation.book.GetBooksCommand;
+import dao.BookDao;
+import dao.implementation.BookDaoImpl;
+import domain.Book;
 import org.apache.struts2.ServletActionContext;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 public class GetBooksAction extends ActionSupport {
     public GetBooksAction() { }
+
+    private Book[] books;
+
+    public String getJSONBooks(){
+        System.out.println("Books was requested");
+       BookDao bookDao = BookDaoImpl.getInstance();
+        List<Book> list = bookDao.getBooksList();
+        this.books = list.toArray(new Book[list.size()]);
+        return SUCCESS;
+    }
 
     @Override
     public String execute() throws Exception {
@@ -16,5 +30,13 @@ public class GetBooksAction extends ActionSupport {
             return ERROR;
         }
         return SUCCESS;
+    }
+
+    public Book[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
     }
 }
---
+++ web/assets/views/footer.html
@@ -0,0 +1,6 @@
+<footer class="page-footer" id="footer">
+    <div class="container grey-text">
+        <span class="left">Copyright 2016</span>
+        <span class="right">Made by LibraryServiceCompany</span>
+    </div>
+</footer>
